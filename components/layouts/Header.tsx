f42c4a06daf6c1f4c7d5dece42924445b88a1c32
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-01-24 13:49:31 +0900

    feat: header component

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -1,5 +1,24 @@
+import HeaderLogo from "@/public/assets/image/logo/headerLogo.svg"
+import Triangle from "@/public/assets//image/triangle/header.svg"
+import styled from "@emotion/styled"
+
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <HeaderWrapper>
+      <Triangle />
+      <HeaderLogo height={32} width={185} />
+    </HeaderWrapper>
+  )
 }
 
 export default Header
+
+const HeaderWrapper = styled.div`
+  height: 72px;
+  background-color: var(--color-yellow-01);
+  display: flex;
+  flex-direction: row;
+  padding-right: 32px;
+  justify-content: space-between;
+  align-items: center;
+`
