dfaa1d1ec0b570d2c580aa9605af63fad3bed42e
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-01-30 13:22:14 +0900

    style: 회원가입 뒷배경 추가

--- components/pages/signup/index.tsx
+++ components/pages/signup/index.tsx
@@ -1,12 +1,20 @@
 import SignupForm from "@/components/forms/SignupForm"
+import BackgroundLayout from "@/components/layouts/BackgroundLayout"
 import AuthTitle from "@/components/molecules/AuthTitle"
 
 const Signup = () => {
   return (
-    <>
+    <BackgroundLayout
+      imgSrc={"/assets/background/triangle.svg"}
+      startColor="--gradient-yellow"
+      endColor="--color-blue-01"
+      degree="180deg"
+      imgWidth={430}
+      imgHeight={849}
+    >
       <AuthTitle text="SIGNUP" />
       <SignupForm />
-    </>
+    </BackgroundLayout>
   )
 }
 
