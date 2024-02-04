fd24b0874d3cc78e7ef2626dcacb3e38c4e16158
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-02-04 15:24:05 +0900

    feat: add Make game board logic

---
+++ utils/gameBoard.ts
@@ -0,0 +1,97 @@
+export type BoardType = "4x4"
+export type Board = Array<Array<number>>
+type boardSpots = {
+  [randomSpot: string]: { x: number; y: number }
+}
+
+export const getRandomNumnber = (max_: number, min_: number): number => {
+  return Math.floor(Math.random() * max_) + min_
+}
+export const getRandomArray = (
+  max_: number,
+  min_: number,
+  count: number
+): Array<number> => {
+  const randomArray: number[] = []
+  while (randomArray.length < count) {
+    const num = getRandomNumnber(max_, min_)
+    if (!randomArray.includes(num)) {
+      randomArray.push(num)
+    }
+  }
+
+  return randomArray
+}
+
+export const makeGameBoard = (type: BoardType = "4x4"): Board => {
+  let ROWS: number
+  let COLS: number
+
+  if (type === "4x4") {
+    ROWS = 4
+    COLS = 4
+  } else {
+    // 추후 처리 필요
+    ROWS = 4
+    COLS = 4
+  }
+
+  const Board: Board = []
+
+  for (let r = 0; r < ROWS; r++) {
+    Board.push([])
+    for (let c = 0; c < COLS; c++) {
+      Board[r].push(0)
+    }
+  }
+
+  return insertRandomNumbers(Board)
+}
+
+export const insertRandomNumbers = (Board: Board): Board => {
+  let computedBoard = Board
+  let totalSpots: number
+  let maxNumber: number
+
+  if (computedBoard.length === 4) {
+    totalSpots = 4 * 4
+    maxNumber = 4
+  } else {
+    totalSpots = 4
+    maxNumber = 4
+  }
+
+  const randomSpots: boardSpots = {}
+
+  while (true) {
+    if (Object.keys(randomSpots).length === totalSpots) {
+      break
+    }
+
+    const x = getRandomNumnber(maxNumber, 0)
+    const y = getRandomNumnber(maxNumber, 0)
+
+    const randomSpotKey = Math.random()
+
+    if (!Object.values(randomSpots).find((val) => val.x === x && val.y === y)) {
+      randomSpots[randomSpotKey] = { x, y }
+    }
+
+    let counter = 2
+    const spotMax_ = 30
+    const spotMin_ = 1
+    const currNumber = getRandomArray(spotMax_, spotMin_, totalSpots / 2)
+
+    Object.values(randomSpots).forEach((spot, idx) => {
+      computedBoard[spot.x][spot.y] = currNumber[idx]
+      counter--
+      if (counter === 0) {
+        counter = 2
+      }
+    })
+  }
+
+  console.log(computedBoard)
+
+  return Board
+}
