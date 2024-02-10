2d6edda024e5f0a1209d6f62cc5ab07bcbec8d53
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-02-11 05:39:35 +0900

    feat: add island redux management

---
+++ redux/common/islandSlice.ts
@@ -0,0 +1,69 @@
+import { AppState } from "@/redux/store/store"
+import { IIsland } from "@/types/common/islandProps"
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+
+const initialState: { island: IIsland } = {
+  island: {
+    background: 0,
+    items: {
+      loc_0: {},
+      loc_1: {},
+      loc_2: {},
+      loc_3: {},
+
+      loc_4: {},
+      loc_5: {},
+      loc_6: {},
+      loc_7: {},
+
+      loc_8: {},
+      loc_9: {},
+
+      loc_10: {},
+
+      loc_11: {},
+      loc_12: {},
+
+      loc_13: {},
+
+      loc_14: {},
+      loc_15: {},
+      loc_16: {},
+      loc_17: {},
+
+      loc_18: {},
+      loc_19: {},
+
+      loc_20: {},
+      loc_21: {},
+      loc_22: {},
+      loc_23: {},
+      loc_24: {},
+    },
+  },
+}
+export const islandSlice = createSlice({
+  name: "island",
+  initialState,
+  reducers: {
+    setIslandBackground: (
+      state,
+      action: PayloadAction<IIsland["background"]>
+    ) => {
+      state.island.background = action.payload
+    },
+    setIslandItems: (state, action: PayloadAction<IIsland["items"]>) => {
+      state.island.items = action.payload
+    },
+  },
+})
+
+export const { setIslandItems, setIslandBackground } = islandSlice.actions
+
+export const selectIslandBackground = (state: AppState) =>
+  state.island.island.background
+export const selectIslandItems = (state: AppState) => state.island.island.items
+export const selectIslandItem0 = (state: AppState) =>
+  state.island.island.items.loc_0
+
+export default islandSlice.reducer
---
+++ types/common/islandProps.ts
@@ -0,0 +1,40 @@
+interface IIsland {
+  background: 0 | 1 | 2
+  items: {
+    loc_0: object
+    loc_1: object
+    loc_2: object
+    loc_3: object
+
+    loc_4: object
+    loc_5: object
+    loc_6: object
+    loc_7: object
+
+    loc_8: object
+    loc_9: object
+
+    loc_10: object
+
+    loc_11: object
+    loc_12: object
+
+    loc_13: object
+
+    loc_14: object
+    loc_15: object
+    loc_16: object
+    loc_17: object
+
+    loc_18: object
+    loc_19: object
+
+    loc_20: object
+    loc_21: object
+    loc_22: object
+    loc_23: object
+    loc_24: object
+  }
+}
+
+export type { IIsland }
