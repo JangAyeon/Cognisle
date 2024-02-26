2e642a1d739cb492389b4b7727f401324227b927
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-02-26 19:14:06 +0900

    refactor: move card Image shown function to utils

--- components/atoms/card/GameCard.tsx
+++ components/atoms/card/GameCard.tsx
@@ -2,17 +2,16 @@ import Image from "next/image"
 
 import { ICard } from "@/types/common/gameProps"
 
-const getCardSrc = (state: ICard["state"], value: number) => {
-  if (state === "hidden") {
-    return "/assets/card/hidden.png"
-  } else {
-    return `/assets/card/reveal/item_${value}.png`
-  }
-}
+import { getCardImage } from "@/utils/gameBoard"
 
 const GameCard = ({ state, value }: ICard) => {
   return (
-    <Image src={getCardSrc(state, value)} alt={state} width={82} height={124} />
+    <Image
+      src={getCardImage(state, value)}
+      alt={state}
+      width={82}
+      height={124}
+    />
   )
 }
 
