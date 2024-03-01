ad4167fd8e3de1f21524568610ba0c9dfa512214
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-03-02 02:25:45 +0900

    feat: visit과 island로 섬 보여주는 거 분리

--- pages/visit/index.tsx
+++ pages/visit/index.tsx
@@ -1,10 +1,34 @@
 import styled from "@emotion/styled"
-import { useEffect, useRef } from "react"
+import { User } from "@supabase/supabase-js"
+import { useRouter } from "next/router"
+import { useCallback, useEffect, useRef, useState } from "react"
 
 import BackgroundLayout from "@/components/layouts/BackgroundLayout"
-import VisitContainer from "@/components/pages/vist"
+import LandLoading from "@/components/molecules/LandLoading"
+import Island_ from "@/components/pages/island"
+import VisitRequest from "@/components/pages/vist"
+
+import useIsland from "@/hooks/useIsland"
+
+import { getIslandInfo } from "@/utils/island"
 
 const Island = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const { islandType, islandItemExist } = useIsland()
+  const {
+    query: { id, name },
+  } = useRouter()
+  const stopLoading = async () => {
+    await new Promise((resolve) => setTimeout(resolve, 3000))
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    if (isLoading && islandItemExist) {
+      console.log("stop")
+      stopLoading()
+    }
+  }, [islandType, islandItemExist, isLoading])
   return (
     <PageWrapper>
       <BackgroundLayout
@@ -15,7 +39,7 @@ const Island = () => {
         endColor="--color-green-03"
         degree="180deg"
       >
-        <VisitContainer />
+        {!id ? <VisitRequest /> : isLoading ? <LandLoading /> : <Island_ />}
       </BackgroundLayout>
     </PageWrapper>
   )
